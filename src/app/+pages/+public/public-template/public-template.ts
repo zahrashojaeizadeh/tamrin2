283f8a7996077212486f573c45dea6140863bfae
zahra shogaei zadeh <zahrash3277@gmail.com>
2025-11-20 06:46:38 -0800

    Pages Creation

---
+++ src/app/+pages/+public/public-template/public-template.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-template',
+  imports: [],
+  templateUrl: './public-template.html',
+  styleUrl: './public-template.scss',
+})
+export class PublicTemplate {
+
+}
