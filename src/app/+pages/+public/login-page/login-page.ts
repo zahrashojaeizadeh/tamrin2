98d4b1306c0e1b2e5e755bfc4542b5cbfd0137c8
zahra shogaei zadeh <zahrash3277@gmail.com>
2025-11-21 14:57:51 -0800

    Routes Configurations

--- src/app/+pages/+public/login-page/login-page.ts
+++ src/app/+pages/+public/login-page/login-page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
-
+  username:string='';
+  password:string='';
+  remember:boolean=false;
+  router=inject(Router);
+  message:string='';
+  check(){
+if (this.username=='admin' && this.password=='admin') {
+    this.router.navigateByUrl('/private')
+} else {
+    this.message='نام کاربری یا کلمه عبور صحیح نیست'
+}
+  }
 }
